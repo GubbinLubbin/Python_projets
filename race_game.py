5cfc35d07911770eaf051c56e3e3ab6c5df405a9
GubbinLubbin <silwalankit27@gmail.com>
2025-06-28 07:18:28 +0530

    Update race_game.py

--- race_game.py
+++ race_game.py
@@ -1,36 +1,36 @@
 import random
 from turtle import Turtle,Screen
-is_race_on=False
+message_turtle = Turtle()
+message_turtle.hideturtle()
+message_turtle.penup()
+message_turtle.goto(0, 0) 
+message_turtle.color("white") 
+is_race_on=True
+participants={"tim":"red","tommy":"pink","timmy":"yellow","tina":"purple","tuna":"blue"}
 screen=Screen()
-screen.setup(width=500,height=400)
-user_bet=screen.textinput(title="Make your bet:",prompt="Enter which color do you think gonna win the race:")
-turtles=["timmy",'tommy','tom','tim',"lumber"]
-colors=["red",'orange','blue','purple','black']
-y_axis=180
-all_turtles=[]
-for i,j in zip(turtles,colors):
-    i=Turtle()
-    i.shape("turtle")
-    i.color(j)
-    i.pensize(5)
-    i.penup()
-    i.goto(x=-220,y=y_axis)
-    y_axis=y_axis-80
-    all_turtles.append(i)
-if user_bet:
-    is_race_on=True
+user_bet=screen.textinput(title="Make your bet:",prompt="Which colour do you think is gonna win red,pink,yellow,purple,blue")
+screen.setup(600,600)
+screen.title("Freaking race game")
+screen.bgcolor("Black")
+cordinates=[(-280,0),(-280,120),(-280,240),(-280,-120),(-280,-240)]
+racers=[]
+for name,cor in zip(participants,cordinates):
+    turtle=Turtle()
+    turtle.shape("turtle")
+    turtle.color(participants[name])
+    turtle.penup()
+    turtle.goto(cor)
+    racers.append(turtle)
 while is_race_on:
-    for turtle in all_turtles:
-        rand_distance=random.randint(0,10)
-        turtle.forward(rand_distance)
-        if(turtle.xcor()>200):
-            winner_color=turtle.pencolor()
+    for turtles in racers:
+        randomdistance=random.randint(1,10)
+        turtles.forward(randomdistance)
+        if(turtles.xcor()>280):
             is_race_on=False
-            result_writer = Turtle()
-            result_writer.hideturtle()
-            result_writer.penup()
-            result_writer.goto(0, 0)
-            result_writer.color("green" if winner_color == user_bet else "red")
-            message = "You won! 🎉" if winner_color == user_bet else f"You lost. 😞 The winner was {winner_color}."
-            result_writer.write(message, align="center", font=("Arial", 16, "bold"))
+            winner=turtles.pencolor()
+            if user_bet.lower() == winner.lower():
+                message_turtle.write(" You Won! ")
+            else:
+                message_turtle.write(" You Lost!")
 screen.exitonclick()
+

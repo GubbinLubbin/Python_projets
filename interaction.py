75079e11205d7c210eb652c51a8fd61d119ea07d
GubbinLubbin <silwalankit27@gmail.com>
2025-09-29 00:21:35 +0530

    Add files via upload

--- interaction.py
+++ interaction.py
@@ -1,9 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 driver_options = webdriver.ChromeOptions()
 driver_options.add_experimental_option("detach", True)
-webdriver=webdriver.Chrome(options=driver_options)
-webdriver.get("https://en.wikipedia.org/wiki/Main_Page")
-active_editors=webdriver.find_element(By.CSS_SELECTOR,"#articlecount a")
-print(active_editors.text)
-webdriver.close()
+driver=webdriver.Chrome(options=driver_options)
+driver.get("http://secure-retreat-92358.herokuapp.com")
+first_name=driver.find_element(By.NAME,value="fname")
+first_name.send_keys("Ankit")
+last_name=driver.find_element(By.NAME,value="lname")
+last_name.send_keys("Silwal")
+gmail=driver.find_element(By.NAME,value="email")
+gmail.send_keys("studypurposes911@gmail.com")
+btn=driver.find_element(By.CSS_SELECTOR,"formbtn")
+btn.click()
